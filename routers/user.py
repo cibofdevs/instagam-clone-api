6d0e5d5abf44bdd2f5ebbc2db4ac2407de2f1f2f
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-10 15:50:28 +0700

    add create user router

---
+++ routers/user.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from db.database import get_db
+from db import db_user
+from routers.schemas import UserDisplay, UserBase
+
+router = APIRouter(
+    prefix="/user",
+    tags=["user"]
+)
+
+
+@router.post("", response_model=UserDisplay)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
+    return db_user.create_user(db, request)
