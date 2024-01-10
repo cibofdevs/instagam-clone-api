2431fdba4676052277287057efb79b98e0c8ffb8
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-10 15:21:40 +0700

    add user model

---
+++ db/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from db.database import Base
+
+
+class DbUser(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
