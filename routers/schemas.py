723a3f362761e485497a9b5b68a57bfc748de281
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 00:00:48 +0700

    add posts schemas

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
@@ -10,6 +12,32 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+
+    class Config:
+        orm_mode = True
+
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
+
+
+class User(BaseModel):
+    username: str
+
+    class Config:
+        orm_mode = True
+
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
 
     class Config:
         orm_mode = True
