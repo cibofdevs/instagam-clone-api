b3cf3b6743ccee59ea5fd68c958d56ba4ca56344
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-10 15:49:39 +0700

    add create user functionality

---
+++ db/db_user.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm.session import Session
+
+from db.models import DbUser
+from routers.schemas import UserBase
+
+
+def create_user(db: Session, request: UserBase):
+    new_user = DbUser(
+        username=request.username,
+        email=request.email,
+        password=request.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
