c9865139b9269383f465f635bd5d3eb410a58447
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 00:01:27 +0700

    add create post query

---
+++ db/db_post.py
@@ -0,0 +1,20 @@
+import datetime
+
+from sqlalchemy.orm import Session
+
+from db.models import DbPost
+from routers.schemas import PostBase
+
+
+def create(db: Session, request: PostBase):
+    new_post = DbPost(
+        image_url=request.image_url,
+        image_url_type=request.image_url_type,
+        caption=request.caption,
+        timestamp=datetime.datetime.now(),
+        user_id=request.creator_id
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
