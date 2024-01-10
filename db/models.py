34d1db77356bd6248eb53167a70f878f1abc20c6
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 00:01:57 +0700

    add table post

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 from db.database import Base
 
@@ -9,3 +10,15 @@ class DbUser(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+    items = relationship("DbPost", back_populates="user")
+
+
+class DbPost(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True, index=True)
+    image_url = Column(String)
+    image_url_type = Column(String)
+    caption = Column(String)
+    timestamp = Column(DateTime)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("DbUser", back_populates="items")
