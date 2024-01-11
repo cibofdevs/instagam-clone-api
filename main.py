01ee9569fd54de8349559b07d68059b0c501264b
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 17:22:06 +0700

    add cors configuration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth import authentication
 from db import models
@@ -17,6 +18,19 @@ app.include_router(comment.router)
 def root():
     return "Hello World!"
 
+
+# cors configuration
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # generate database
 models.Base.metadata.create_all(engine)
