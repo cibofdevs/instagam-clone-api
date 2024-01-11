ab794a96c5c68eac7d275a41eb419368817ba109
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 10:52:48 +0700

    add authentication to create post and upload image

--- routers/post.py
+++ routers/post.py
@@ -6,9 +6,10 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File
 from sqlalchemy.orm import Session
 
+from auth.oauth2 import get_current_user
 from db import db_post
 from db.database import get_db
-from routers.schemas import PostBase, PostDisplay
+from routers.schemas import PostBase, PostDisplay, UserAuth
 
 router = APIRouter(
     prefix="/post",
@@ -19,7 +20,7 @@ image_url_types = ["absolute", "relative"]
 
 
 @router.post("", response_model=PostDisplay)
-def create(request: PostBase, db: Session = Depends(get_db)):
+def create(request: PostBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
     if not request.image_url_type in image_url_types:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
@@ -33,7 +34,7 @@ def posts(db: Session = Depends(get_db)):
 
 
 @router.post("/image")
-def upload_image(image: UploadFile = File(...)):
+def upload_image(image: UploadFile = File(...), current_user: UserAuth = Depends(get_current_user)):
     letters = string.ascii_letters
     rand_str = "".join(random.choice(letters) for i in range(6))
     new = f"_{rand_str}."
