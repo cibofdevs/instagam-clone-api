024889bef9e291e86e9f71b83193a3476cf16c4c
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 16:25:41 +0700

    add delete router

--- routers/post.py
+++ routers/post.py
@@ -47,3 +47,8 @@ def upload_image(image: UploadFile = File(...), current_user: UserAuth = Depends
     return {
         "filename": path
     }
+
+
+@router.get("/delete/{id}")
+def delete(id: int, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
+    return db_post.delete(db, id, current_user.id)
