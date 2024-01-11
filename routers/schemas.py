f28a7a49f987ae46c7eef2d73c0874007c142918
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 17:14:05 +0700

    add comment schemas

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 from pydantic import BaseModel
 
@@ -31,6 +32,15 @@ class User(BaseModel):
         orm_mode = True
 
 
+class Comment(BaseModel):
+    text: str
+    username: str
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
+
+
 class PostDisplay(BaseModel):
     id: int
     image_url: str
@@ -38,6 +48,7 @@ class PostDisplay(BaseModel):
     caption: str
     timestamp: datetime
     user: User
+    comments: List[Comment]
 
     class Config:
         orm_mode = True
@@ -47,3 +58,9 @@ class UserAuth(BaseModel):
     id: int
     username: str
     email: str
+
+
+class CommentBase(BaseModel):
+    username: str
+    text: str
+    post_id: int
