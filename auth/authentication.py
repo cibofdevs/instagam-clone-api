85daff0a092dc1601ff35e79e050832341441cd0
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 10:51:30 +0700

    add authentication router

---
+++ auth/authentication.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from auth.oauth2 import create_access_token
+from db.database import get_db
+from db.hashing import Hash
+from db.models import DbUser
+
+router = APIRouter(
+    tags=["authentication"]
+)
+
+@router.post("/login")
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(DbUser).filter(DbUser.username == request.username).first()
+    if not user:
+        HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
+    if not Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect password")
+
+    access_token = create_access_token(data={"username": user.username})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_id": user.id,
+        "username": user.username
+    }
