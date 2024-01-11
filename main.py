7b6191b60615f3c173258a795b4e8469bd1169ab
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 10:53:31 +0700

    add authentication router to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from auth import authentication
 from db import models
 from db.database import engine
 from routers import user, post
@@ -8,6 +9,7 @@ from routers import user, post
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(post.router)
+app.include_router(authentication.router)
 
 
 @app.get("/")
