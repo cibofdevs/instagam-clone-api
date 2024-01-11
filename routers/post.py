e431cca26ce8127eaf91bc84189327ab8032c134
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-11 09:42:35 +0700

    add upload image router

--- routers/post.py
+++ routers/post.py
@@ -1,6 +1,9 @@
+import random
+import shutil
+import string
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, UploadFile, File
 from sqlalchemy.orm import Session
 
 from db import db_post
@@ -27,3 +30,19 @@ def create(request: PostBase, db: Session = Depends(get_db)):
 @router.get("/all", response_model=List[PostDisplay])
 def posts(db: Session = Depends(get_db)):
     return db_post.get_all(db)
+
+
+@router.post("/image")
+def upload_image(image: UploadFile = File(...)):
+    letters = string.ascii_letters
+    rand_str = "".join(random.choice(letters) for i in range(6))
+    new = f"_{rand_str}."
+    filename = new.join(image.filename.rsplit(".", 1))
+    path = f"images/{filename}"
+
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {
+        "filename": path
+    }
